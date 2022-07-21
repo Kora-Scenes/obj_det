5abcb3a3ef444561fdcd59810ce5aee5f1d089ac
NikhilAdyapak <56066146+NikhilAdyapak@users.noreply.github.com>
2022-07-21 12:56:18 +0530

    Update obj_det.py

--- obj_det.py
+++ obj_det.py
@@ -462,18 +462,18 @@ class obj_det_evaluator:
 			# mlflow.log_metric("confusion", yolo_metrics)
 			# mlflow.log_metric(str(key),value)
 
-			tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+# 			tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
-			# Model registry does not work with file store
-			if tracking_url_type_store != "file":
+# 			# Model registry does not work with file store
+# 			if tracking_url_type_store != "file":
 
-				# Register the model
-				# There are other ways to use the Model Registry, which depends on the use case,
-				# please refer to the doc for more information:
-				# https://mlflow.org/docs/latest/model-registry.html#api-workflow
-				mlflow.sklearn.log_model(self.model, "model", registered_model_name="ElasticnetWineModel")
-			else:
-				mlflow.sklearn.log_model(self.model, "model")
+# 				# Register the model
+# 				# There are other ways to use the Model Registry, which depends on the use case,
+# 				# please refer to the doc for more information:
+# 				# https://mlflow.org/docs/latest/model-registry.html#api-workflow
+# 				mlflow.sklearn.log_model(self.model, "model", registered_model_name="ElasticnetWineModel")
+# 			else:
+# 				mlflow.sklearn.log_model(self.model, "model")
 
 		print(results)
 		preds1 = preds
